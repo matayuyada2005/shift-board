af1ebaf93c431fc034ae7fac3e634d4920e0165f
matayuyada2005 <166238482+matayuyada2005@users.noreply.github.com>
2026-07-12 14:01:00 +0900

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,7 @@
-// ここに、あなた自身のFirebaseプロジェクトの設定値を入れてください。
-// 取得方法は README.md の「Firebaseのセットアップ」を参照してください。
-//
-// Firebaseコンソール > プロジェクトの設定 > 全般 > マイアプリ > SDKの設定と構成
-// に表示される firebaseConfig をそのままコピーして貼り付ければOKです。
+// js/firebase-config.js
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// あなた自身のFirebaseプロジェクトの設定値
+export const firebaseConfig = {
   apiKey: "AIzaSyBZPH_LPVx0I5ecONp-n12KDhMpDVt0V5o",
   authDomain: "quonsuntory-ptj.firebaseapp.com",
   projectId: "quonsuntory-ptj",
@@ -21,7 +10,3 @@ const firebaseConfig = {
   appId: "1:456825515609:web:17e37b2323fcace4444b48",
   measurementId: "G-TEL1Z4X6JZ"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
